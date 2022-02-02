3917f130f9ff771ba440df52fafef798fa887a22
Lindsay <lbtannahill@gmail.com>
2022-02-01 17:38:31 -0800

    avatar and tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,53 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// /*
+//  * Client-side JS logic goes here
+//  * jQuery is already loaded
+//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+//  */
 
+// $(document).ready(function() {
+
+//   createTweetElement;
+
+//   const $tweet = $(`<article class="tweet">Hello world</article>`);
+
+
+//   // Within the client.js file, we're going to define a function createTweetElement that takes in a 
+//   // tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
+
+
+// });
+
+
+
+// // Test / driver code (temporary). Eventually will get this from the server.
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
+
+// const $tweet = createTweetElement(tweetData);
+
+// // Test / driver code (temporary)
+// console.log($tweet); // to see what it looks like
+// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right element
+
+
+
+// {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+
+  $('textarea').keyup(function() {
+    
+    let characterCount = $(this).val().length,
+      current = $('#current'),
+      theCount = $('#the-count');
+      
+    current.text(characterCount);
+   
+    if (characterCount >= 141) {
+      current.css('color', '#8f0001');
+      current.text(140 - characterCount);
+    }
+  });
+});
+
+
