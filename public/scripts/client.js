e9759113dbf6a9bdcb12dff80913ef0dc1beef0f
Lindsay <lbtannahill@gmail.com>
2022-02-03 10:30:26 -0800

    trying out jquer

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,53 +1,48 @@
-// /*
-//  * Client-side JS logic goes here
-//  * jQuery is already loaded
-//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-//  */
+/*
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
 
-// $(document).ready(function() {
+$(document).ready(function() {
 
-//   createTweetElement;
+  createTweetElement;
 
-//   const $tweet = $(`<article class="tweet">Hello world</article>`);
+  const $tweet = $(`<article class="tweet">Hello world</article>`);
 
 
-//   // Within the client.js file, we're going to define a function createTweetElement that takes in a 
-//   // tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
+  // Within the client.js file, we're going to define a function createTweetElement that takes in a 
+  // tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
 
 
 // });
 
 
 
-// // Test / driver code (temporary). Eventually will get this from the server.
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
-
-// const $tweet = createTweetElement(tweetData);
-
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right element
 
 
 
-// {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
+
+
+// notes from Andy lecture 
+
+
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right element
+
+// const $species = $('<h2>').text('OgSpecifies: Human'); 
+// const $header = $('<header>').addClass('header'); 
+
+// $header.append($species).append($colour); 
+// shorthand for append 
+// $header.append($species, $colour)
+
+
+// This is a security concern 
+// const $section = $(`
+// <section class="body"> aksjdfl;askdjfasldkj;f </section>
+// `) ;
