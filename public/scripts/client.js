c0723914f1cc54abe09bf5fefa6c0d4f0214364e
Lindsay <lbtannahill@gmail.com>
2022-02-03 22:08:29 -0800

    function that creations tweet from object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,20 +4,46 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+
+
+
+
 $(() => {
-  const $avatars = $(`<i class="fa-solid fa-crow"></i>`).addClass('avatars');
-  const $name = $('<div>').text(`Ms. Crow`).addClass('name');
-  const $handle = $('<div>').text('@CrowFacts').addClass('handle lightcolor');
+
+
+  const createTweetElement = function(inputData) {
+
+  const $picture = $(`<img src="${tweetData.user.avatars}">`);
+
+  const $avatars = $($picture).addClass('avatars');
+  const $name = $('<div>').text(`${tweetData.user.name}`).addClass('name');
+  const $handle = $('<div>').text(`${tweetData.user.handle}`).addClass('handle lightcolor');
   const $user = $('<header>').addClass('user tweetheader');
   $user.append($avatars, $name, $handle);
   
   //create body 
-  const $text = $('<div>').text(`Tweet written from input above.`).addClass('text');
+  const $text = $('<div>').text(`${tweetData.content.text}`).addClass('text');
   const $content = $('<section>').addClass('content tweetmain');
   $content.append($text);
   
   // create footer
-  const $created_at = $('<div>').text('1 year ago').addClass('created_at margin-top');
+    let $timeAgo = timeago.format(tweetData.created_at);  
+
+  const $created_at = $('<div>').text($timeAgo).addClass('created_at margin-top');
+  
   const $icons1 = $(`<i class="fa-solid fa-flag icon">`).addClass('right margin-top')
   const $icons2 = $(`</i><i class="fa-solid fa-retweet icon"></i>`).addClass('margin-top')
   const $icons3 = $(`<i class="fa-regular fa-heart icon"></i>`).addClass('margin-top')
@@ -37,7 +63,19 @@ $(() => {
   const $tweetcontainer = $('#tweetcontainer')
  
   $tweetcontainer.append($tweetbox)
+  }
   
+
+  const $tweet = createTweetElement(tweetData);
+
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+
+
+
+
+
 });
 
 
