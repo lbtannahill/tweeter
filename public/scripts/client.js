91a7b2612a554c1f210778118473f20403d0830e
Lindsay <lbtannahill@gmail.com>
2022-02-03 16:55:01 -0800

    updated stuff

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,45 +4,44 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
+$(() => {
+  const $avatars = $(`<i class="fa-solid fa-crow"></i>`).addClass('avatars');
+  const $name = $('<div>').text(`Ms. Crow`).addClass('name');
+  const $handle = $('<div>').text('@CrowFacts').addClass('handle lightcolor');
+  const $user = $('<header>').addClass('user tweetheader');
+  $user.append($avatars, $name, $handle);
+  
+  //create body 
+  const $text = $('<div>').text(`Tweet written from input above.`).addClass('text');
+  const $content = $('<section>').addClass('content tweetmain');
+  $content.append($text);
+  
+  // create footer
+  const $created_at = $('<div>').text('1 year ago').addClass('created_at margin-top');
+  const $icons1 = $(`<i class="fa-solid fa-flag icon">`).addClass('right margin-top')
+  const $icons2 = $(`</i><i class="fa-solid fa-retweet icon"></i>`).addClass('margin-top')
+  const $icons3 = $(`<i class="fa-regular fa-heart icon"></i>`).addClass('margin-top')
+  const $tweetfooter = $('<footer>').addClass('tweetfooter smallfont');
+  $tweetfooter.append($created_at, $icons1, $icons2, $icons3);
+  
+  
+  // create artcle
+  
+  const $tweetarticle = $('<article>').addClass('tweetData articlebox');
+    $tweetarticle.append($user, $content, $tweetfooter)
 
-  createTweetElement;
-
-  const $tweet = $(`<article class="tweet">Hello world</article>`);
-
-
-  // Within the client.js file, we're going to define a function createTweetElement that takes in a 
-  // tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
-
-
-// });
-
+  // create section
+  const $tweetbox = $('<section>').addClass('tweets')
+    $tweetbox.append($tweetarticle)
+  
+  const $tweetcontainer = $('#tweetcontainer')
+ 
+  $tweetcontainer.append($tweetbox)
+  
+});
 
 
 
 
 
 
-
-// notes from Andy lecture 
-
-
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right element
-
-// const $species = $('<h2>').text('OgSpecifies: Human'); 
-// const $header = $('<header>').addClass('header'); 
-
-// $header.append($species).append($colour); 
-// shorthand for append 
-// $header.append($species, $colour)
-
-
-// This is a security concern 
-// const $section = $(`
-// <section class="body"> aksjdfl;askdjfasldkj;f </section>
-// `) ;
